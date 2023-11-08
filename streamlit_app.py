801a4ae4f90e18b6a4a3818caff0b1e9ad868354
JesusPaucar <42385952+JesusPaucar@users.noreply.github.com>
2023-11-08 19:18:31 -0300

    Add files via upload
    Mi primera version de la app

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  8 18:57:34 2023
+
+@author: Jesus
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+n = st.slider('n', 5, 100, step = 1)
+char_data = pd.DataFrame(np.random.rand(n), columns = ['data'])
+st.line_chart(char_data)
